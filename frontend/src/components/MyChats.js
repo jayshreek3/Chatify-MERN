d8b009722518ada0dbde755be5062e7042b92cbd
Jayshree Keskar <jayshreekeskar321@gmail.com>
2024-06-24 03:48:10 +0530

    Added Create New Group Chat feature

--- frontend/src/components/MyChats.js
+++ frontend/src/components/MyChats.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { ChatState } from "../Context/ChatProvider";
-import { useToast, Box, Button} from "@chakra-ui/react";
+import { useToast, Box, Button, Stack, Text} from "@chakra-ui/react";
 import axios from "axios";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons'; 
+import ChatLoading from "../components/ChatLoading";
+import { getSender } from "../config/ChatLogic";
+import { GroupChatModal } from "./Miscellaneous/GroupChatModal";
 
-const MyChats = () => {
+const MyChats = ({ fetchAgain }) => {
   const { user, selectedChat, setSelectedChat, chats, setChats } = ChatState();
   const [loggedUser, setLoggedUser] = useState(null);
 
@@ -40,7 +43,7 @@ const MyChats = () => {
     if (userInfo) {
       fetchChats();
     }
-  }, []);
+  }, [fetchAgain ]);
 
   return (
     <Box
@@ -63,14 +66,56 @@ const MyChats = () => {
         justifyContent="space-between"
         alignItems="center"
       >My chats
-      <Button
+      <GroupChatModal><Button
             d="flex" fontFamily="Roboto" padding-left="200px" marginLeft="120px"
             fontSize={{ base: "16px", md: "20px", lg: "17px" }}
             
             
           > <FontAwesomeIcon icon={faPlus} />
               New Group Chat
-          </Button>
+          </Button></GroupChatModal>
+      
+      </Box>
+      <Box  d="flex"
+        flexDir="column"
+        p={3}
+        bg="#F8F8F8"
+        w="100%"
+        h="90%"
+        borderRadius="lg"
+        overflowY="hidden">
+          {chats ? (
+          <Stack overflowY="hidden">
+            {chats.map((chat) => (
+              <Box
+                onClick={() => setSelectedChat(chat)}
+                cursor="pointer"
+                bg={selectedChat === chat ? "#e1ad01" : "#E8E8E8"}
+                color={selectedChat === chat ? "black" : "black"}
+                px={3}
+                py={2}
+                borderRadius="lg"
+                key={chat._id}
+              >
+                <Text>
+                  {!chat.isGroupChat
+                    ? getSender(loggedUser, chat.users)
+                    : chat.chatName}
+                </Text>
+                {chat.latestMessage && (
+                  <Text fontSize="xs">
+                    <b>{chat.latestMessage.sender.name} : </b>
+                    {chat.latestMessage.content.length > 50
+                      ? chat.latestMessage.content.substring(0, 51) + "..."
+                      : chat.latestMessage.content}
+                  </Text>
+                )}
+              </Box>
+            ))}
+          </Stack>
+        ) : (
+          <ChatLoading />
+        )}
       </Box>
     </Box>
   );
